6b43d48803df3bc503f96e4812b56a09cee8f9f7
stephanyvlz <5323100091@correcaminos.uts.edu.mx>
2024-10-16 12:36:29 -0700

    Agregar archivos de proyecto.

---
+++ frmYoutube.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BLL;
+using DAL;
+
+namespace YoutubeAPI
+{
+    public partial class frmYoutube : System.Web.UI.Page
+    {
+        protected async void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string query = txtBuscar.Text;
+            BDYoutube_DAL youtubeDAL = new BDYoutube_DAL();
+            List<BDYoutube_BLL> videos = await youtubeDAL.BuscarVideos(query);
+            rptVideos.DataSource = videos;
+            rptVideos.DataBind();
+        }
+    }
+}
